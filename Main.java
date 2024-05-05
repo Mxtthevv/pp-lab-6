c22fff9312d4ec3dfb83f422e5af604167aec307
Mateusz Dziubak <mateusz@MacBook-Pro-Mateusz.local>
2024-05-05 12:57:42 +0200

    zad4

--- Main.java
+++ Main.java
@@ -9,22 +9,67 @@ public class Main {
         Worker worker1 = new Worker("Adam", 1000, 1, "01.01.2021", "Pracownik");
         Worker worker2 = new Worker("Bartek", 2000, 1, "02.02.2021", "Pracownik");
         Worker worker3 = new Worker("Czarek", 3000, 3, "03.03.2021", "Pracownik");
-        Worker worker4 = new Worker("Damian", 4000, 5, "04.04.2021", "Pracownik");
+        Worker worker4 = new Worker("Damian", 4000, 4, "04.04.2021", "Pracownik");
 
         Worker worker5 = new Worker("Filip", 5000, 5, "05.05.2021", "Pracownik");
-        Worker worker6 = new Worker("Grzegorz", 6000, 5, "05.05.2021", "Pracownik");
-        Worker worker7 = new Worker("Halina", 7000, 6, "06.06.2021", "Pracownik");
+        Worker worker6 = new Worker("Grzegorz", 6000, 6, "05.05.2021", "Pracownik");
+        Worker worker7 = new Worker("Halina", 7000, 7, "06.06.2021", "Pracownik");
 
-        Manager manager = new Manager("Eryk", 10000, 7, "05.05.2021", "Kierownik");
-        Manager manager2 = new Manager("Igor", 11000, 8, "08.08.2021", "Kierownik");
+        Worker worker8 = new Worker("Jan", 8000, 8, "07.07.2021", "Pracownik");
 
-        System.out.println(worker5.getName() + " has code: " + worker5.hashCode());
-        System.out.println(worker6.getName() + " has code: " + worker6.hashCode());
-        System.out.println(worker7.getName() + " has code: " + worker7.hashCode());
-        System.out.println(manager2.getName() + " has code: " + manager2.hashCode());
+        Manager manager = new Manager("Eryk", 10000, 1, "05.05.2021", "Kierownik");
+        Manager manager2 = new Manager("Igor", 11000, 10, "08.08.2021", "Kierownik");
 
-        System.out.println(worker6.getName() + " równa się z pracownikiem nr 5: " + worker6.equals(worker5));
-        System.out.println(worker6.getName() + " równa się z pracownikiem nr 6: " + worker6.equals(worker6));
-        System.out.println(worker6.getName() + " równa się z kierownikiem nr 2: " + worker6.equals(manager2));
+        Manager manager3 = new Manager("Kamil", 12000, 1, "09.09.2021", "Kierownik");
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(worker5);
+        employees.add(worker6);
+        employees.add(worker7);
+        employees.add(worker8);
+
+        employees.add(manager);
+        employees.add(manager2);
+        employees.add(manager3);
+
+        // Suma pensji wszystkich pracowników + kierowników //
+        double totalSalaryAllEmployees = employees.stream()
+        .mapToDouble(Employee::getSalary)
+        .sum();
+
+        // Suma pensji wszystkich kierowników //
+        double totalSalaryManagers = employees.stream()
+                .filter(employee -> employee instanceof Manager)
+                .mapToDouble(Employee::getSalary)
+                .sum();
+
+        // Suma pensji wszystkich pracowników //
+        double totalSalaryWorkers = employees.stream()
+                .filter(employee -> employee instanceof Worker)
+                .mapToDouble(Employee::getSalary)
+                .sum();
+
+        System.out.println("Suma pensji wszystkich pracowników + kierowników: " + totalSalaryAllEmployees + " PLN");
+        System.out.println(" ");
+        System.out.println("Suma pensji wszystkich kierowników: " + totalSalaryManagers + " PLN");
+        System.out.println(" ");
+        System.out.println("Suma pensji wszystkich pracowników: " + totalSalaryWorkers + " PLN");
+        System.out.println(" ");
+
+        // Wyszukiwanie pracowników o tym samym ID //
+        for (Employee employee : employees) {
+            long matchingIdCount = employees.stream()
+                    .filter(e -> e.hashCode() == employee.hashCode())
+                    .count();
+
+            if (matchingIdCount > 1) {
+                System.out.println(employee.getName() + " posiada ID który się powtarza z inną zatrudnioną osobą.");
+                System.out.println(" ");
+            }
+        }
     }
 }
