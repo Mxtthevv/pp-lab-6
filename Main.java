25225df38257d55e0166bdb3d82ce46f509da2f9
Mateusz Dziubak <mateusz@MacBook-Pro-Mateusz.local>
2024-05-05 12:11:04 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,33 +1,33 @@
 import company.models.Manager;
 import company.models.Worker;
+import company.abstracts.Employee;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Adam", 1000, 1);
-        Worker worker2 = new Worker("Bartek", 2000, 2);
-        Worker worker3 = new Worker("Czarek", 3000, 3);
-        Worker worker4 = new Worker("Damian", 4000, 4);
+        Worker worker1 = new Worker("Adam", 1000, 1, "01.01.2021", "Pracownik");
+        Worker worker2 = new Worker("Bartek", 2000, 2, "02.02.2021", "Pracownik");
+        Worker worker3 = new Worker("Czarek", 3000, 3, "03.03.2021", "Pracownik");
+        Worker worker4 = new Worker("Damian", 4000, 4, "04.04.2021", "Pracownik");
 
-        Manager manager = new Manager("Eryk", 5000, 5);
+        Manager manager = new Manager("Eryk", 5000, 5, "05.05.2021", "Kierownik");
 
-        System.out.println("Pensja pracownika (nr1): " + worker1.getSalary());
-        worker1.work();
-        System.out.println(" ");
+        List<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(manager);
 
-        System.out.println("Pensja pracownika (nr2): " + worker2.getSalary());
-        worker2.work();
-        System.out.println(" ");
-
-        System.out.println("Pensja pracownika (nr3): " + worker3.getSalary());
-        worker3.work();
-        System.out.println(" ");
-
-        System.out.println("Pensja pracownika (nr4): " + worker4.getSalary());
-        worker4.work();
-        System.out.println(" ");
-
-        System.out.println("Pensja kierownika: " + manager.getSalary());
-        manager.work();
-        System.out.println(" ");
+        for (Employee employee : employees) {
+            employee.work();
+            System.out.println("- " + employee.getName() + 
+                                " (ID: " + employee.hashCode() +
+                                ", Position: " + employee.getPosition() +
+                                ", Hire date: " + employee.getHireDate() +
+                                ", Salary: " + employee.getSalary() + ")");
+            System.out.println(" ");
+        }
     }
 }
